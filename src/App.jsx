0e336106199fb63513b40947c4cd444d3f9ff630
adhiiiiiii <adilmhd8848@gmail.com>
2026-04-21 16:06:49 +0530

    start with Todo-List

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,56 @@
-import { useState } from 'react'
+import { useState,useRef,useEffect } from 'react'
+import TodoList from './TodoList';
+import { v4 as uuidv4 } from 'uuid';
 
-export default function App(){
 
-  const [count , setCount] = useState(0);
+const LOCAL_STORAGE_KEY = 'todoApp=todos'
 
-  function incrementCount(){
-    setCount(prevCount => prevCount+1)    
-  }
+function App(){
 
-  function decrementCount(){
-    setCount(prevCount => prevCount-1)
-  }
-  
+const [todos , setTodos] = useState([])
+const todoNameRef = useRef()
+
+useEffect(() => {
+  const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+  if (storedTodos) setTodos(storedTodos)
+}, [])
+
+useEffect(() => {
+  localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(todos))
+}, [todos])
+
+function toggleTodo(id){
+  const newTodos = [...todos]
+  const todo = newTodos.find(todo => todo.id === id )
+  todo.complete = !todo.complete
+  setTodos(newTodos)
+}
+
+function handleAddTodo(e){
+  const name = todoNameRef.current.value
+  if(name === '') return
+  console.log(name);
+  setTodos(prevTodos => {
+    return[...prevTodos,{id:uuidv4() , name:name , complete:false}]
+  })
+  todoNameRef.current.value = null 
+}
+
+function handleClearTodos(){
+  const newTodos = todos.filter ( todo => !todo.complete)
+  setTodos(newTodos);
+}
 
   return (
     <>
-      <button onClick={decrementCount}>-</button>
-      <span>{count}</span>
-      <button onClick={incrementCount}>+</button>
+      <TodoList todos={todos} toggleTodo={toggleTodo}/>
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <button onClick={handleClearTodos}>Clear complete</button>
+      <div>{todos.filter(todo => !todo.complete).length} left to do</div>
     </>
   )
 }
+
+
+export default App;
